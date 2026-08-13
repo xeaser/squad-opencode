ffe2264bf9805b3a6e6993d16af268ca979caa84
Parag Sharma <paragsharma2106@gmail.com>
2026-08-14 02:55:04 +0530

    Bump version to 0.2.1.

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,7 +1,7 @@
 package version
 
 // Version is the squad-oc release string.
-const Version = "0.2.0"
+const Version = "0.2.1"
 
 // Repo is the public GitHub repo used by update-check.
 const Repo = "xeaser/squad-opencode"
