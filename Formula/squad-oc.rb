9b00f644682fa4e0be8944ff13ce8771f48c0cd1
Parag Sharma <sharmarag93@gmail.com>
2026-08-24 10:26:25 +0530

    chore: pin Homebrew, Scoop, and winget to v0.5.0

--- Formula/squad-oc.rb
+++ Formula/squad-oc.rb
@@ -1,26 +1,26 @@
 class SquadOc < Formula
   desc "Human-led AI agent teams for OpenCode"
   homepage "https://github.com/xeaser/squad-opencode"
-  version "0.4.0"
+  version "0.5.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/xeaser/squad-opencode/releases/download/v0.4.0/squad-oc_0.4.0_darwin_arm64.tar.gz"
-      sha256 "e2c662e3895a7b937aa0539b354fc6705dd56fdb833cd7222d63d07d4afb546b"
+      url "https://github.com/xeaser/squad-opencode/releases/download/v0.5.0/squad-oc_0.5.0_darwin_arm64.tar.gz"
+      sha256 "c1deeb0932e3cbaaee08a92c728845301214af322a2dca01c2d455a9f8ef604e"
     else
-      url "https://github.com/xeaser/squad-opencode/releases/download/v0.4.0/squad-oc_0.4.0_darwin_amd64.tar.gz"
-      sha256 "ee6f9417b71e96904c8522299f8243d46a785b724b66d31c869051d734fd7777"
+      url "https://github.com/xeaser/squad-opencode/releases/download/v0.5.0/squad-oc_0.5.0_darwin_amd64.tar.gz"
+      sha256 "6005c0a55c65edc9c1a7bdde7cc5ebf4668c5097e36f9febb06b705d90dae254"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/xeaser/squad-opencode/releases/download/v0.4.0/squad-oc_0.4.0_linux_arm64.tar.gz"
-      sha256 "e8fa3657fb9b270fc87ef3f24c2236fa751664597245d2976124541933abaf21"
+      url "https://github.com/xeaser/squad-opencode/releases/download/v0.5.0/squad-oc_0.5.0_linux_arm64.tar.gz"
+      sha256 "190803966a61fae6a98729e712ff664e38a1e5189c30f40e3031b1ecbb347ad2"
     else
-      url "https://github.com/xeaser/squad-opencode/releases/download/v0.4.0/squad-oc_0.4.0_linux_amd64.tar.gz"
-      sha256 "c54df6f427bc97b3fbf7a337fe9806e0a29bc69864f9c36133879d24e65a7c0a"
+      url "https://github.com/xeaser/squad-opencode/releases/download/v0.5.0/squad-oc_0.5.0_linux_amd64.tar.gz"
+      sha256 "94dce0b564c81a6e90b1e367cbdee40cfb29fa43a484fe3ac799e61a34f9c869"
     end
   end
 
